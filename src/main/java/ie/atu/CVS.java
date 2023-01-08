2cd56ade129cd6e467d8fd6c44693aeee65dbdd7
Mikaela-D <G00401392@atu.ie>
2023-01-08 23:46:56 +0000

    Added code for CVS

--- src/main/java/ie/atu/CVS.java
+++ src/main/java/ie/atu/CVS.java
@@ -1,4 +1,15 @@
 package ie.atu;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class CVS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new File("names.cvs")); // throws Exception
+        sc.useDelimiter(","); // Sets the delimiter pattern
+        while (sc.hasNext()) { // Returns a boolean value
+            System.out.println(sc.next());
+        }
+        sc.close(); // Closes the scanner
+    }
 }
